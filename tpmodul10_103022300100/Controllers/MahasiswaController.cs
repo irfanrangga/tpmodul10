54fdb70f27ad422f601ed93a9a97fc366179e9a2
Irfan Rangga <irfanrangga04@gmail.com>
2025-04-27 15:00:59 +0700

    menambahkan API Mahasiswa

---
+++ tpmodul10_103022300100/Controllers/MahasiswaController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tpmodul10_103022300100.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        public static readonly List<Mahasiswa> mahasiswaList = new List<Mahasiswa>
+        {
+            new Mahasiswa("Irfan Rangga", "103022300100"),
+            new Mahasiswa("Riza Muhammad", "103022300104"),
+            new Mahasiswa("Tubagus Aulia", "103022300141"),
+            new Mahasiswa("Gamaliel Pradana", "103022300015"),
+            new Mahasiswa("Muhammad Razky", "103022300047"),
+            new Mahasiswa("Christofer Abel", "103022330039")
+        };
+
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return mahasiswaList;
+        }
+
+        [HttpGet("{index}")]
+        public Mahasiswa Get(int index)
+        {
+            return mahasiswaList[index];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa value)
+        {
+            mahasiswaList.Add(value);
+        }
+
+        [HttpDelete("{index}")]
+        public void Delete(int index)
+        {
+            mahasiswaList.RemoveAt(index);
+        }
+    }
+}
---
+++ tpmodul10_103022300100/Mahasiswa.cs
@@ -0,0 +1,13 @@
+﻿namespace tpmodul10_103022300100
+{
+    public class Mahasiswa
+    {
+        public string nama { get; set; }
+        public string nim { get; set; }
+        public Mahasiswa(string nama, string nim)
+        {
+            this.nama = nama;
+            this.nim = nim;
+        }
+    }
+}
